fa5f4a18b3f4724672a95a00a9c9d8c76cc20b31
Marcelo <joaomarcelofurtadoromero@gmail.com>
2023-01-09 23:38:32 -0800

    MarceloBranch: First methods added for the AuthorInterface

--- inc/Api/Data/AuthorInterface.class.php
+++ inc/Api/Data/AuthorInterface.class.php
@@ -2,6 +2,62 @@
 
 namespace inc\Api\Data;
 
+/**
+ * Author interface
+ */
 interface AuthorInterface {
+
+    /**
+     * @return string
+     */
+    public function addOwnBook()                     : string;
+
+    /**
+     * @param int $bookId
+     * @return string
+     */
+    public function editOwnBook(int $bookId)         : string;
+
+    /**
+     * @param int $bookId
+     * @return string
+     */
+    public function deleteOwnBook(int $bookId)       : string;
+
+    /**
+     * @param int $authorId
+     * @return float
+     */
+    public function authorRating(int $authorId)      :? float;
+
+    /**
+     * @param int $authorId
+     * @return int
+     */
+    public function authorNumRating(int $authorId)   : int;
+
+    /**
+     * @param int $authorId
+     * @return int or null
+     */
+    public function authorNumTitles(int $authorId)   :? int;
+
+    /**
+     * @param int $authorId
+     * @return array or null
+     */
+    public function authorTitles(int $authorId)      :? array;
+
+    /**
+     * @param int $authorId
+     * @return string
+     */
+    public function authorDescription(int $authorId) : string;
     
+    /**
+     * @param int $bookId
+     * @return string
+     */
+    public function rentBook(int $bookId)            : string;
+
 }
---
+++ inc/model/Data/Author.class.php
@@ -0,0 +1,8 @@
+<?php
+
+    use inc\Api\Data\AuthorInterface;
+
+    class Author extends User implements AuthorInterface{
+
+        
+    }
